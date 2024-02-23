0a52f4323d4a235ae0b265960f99f915ccc021cb
YusufHasan94 <yhpolok@gmail.com>
2024-02-23 22:48:09 +0600

    implement database and get tasks route

---
+++ dist/index.js
@@ -0,0 +1,59 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongodb_1 = require("mongodb");
+require("dotenv/config");
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+const port = process.env.PORT || 3000;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.kaasp8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new mongodb_1.MongoClient(uri, {
+    serverApi: {
+        version: mongodb_1.ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            yield client.connect();
+            // Send a ping to confirm a successful connection
+            const todo_app = client.db("redux-todo-app").collection("todo-app");
+            app.get("/tasks", (req, res) => __awaiter(this, void 0, void 0, function* () {
+                const result = yield todo_app.find().toArray();
+                console.log(result);
+                res.send(result);
+            }));
+            yield client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        }
+        finally {
+            // Ensures that the client will close when you finish/error
+            // await client.close();
+        }
+    });
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+    res.send("Server working");
+});
+app.listen(port, () => {
+    console.log(`server running on ${port}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import express, { Application, Request, Response } from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
+import cors from "cors";
+
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.kaasp8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const todo_app = client.db("redux-todo-app").collection("todo-app");
+
+    app.get("/tasks", async (req: Request, res: Response) => {
+      const result = await todo_app.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server working");
+});
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
